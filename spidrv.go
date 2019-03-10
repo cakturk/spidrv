77c1f5e1af7eed92c823dbafa33a292bb8edd15a
Cihangir Akturk <cakturk@gmail.com>
2019-03-11 01:20:21 +0300

    clean up some stuff

--- spidrv.go
+++ spidrv.go
@@ -8,40 +8,12 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"periph.io/x/periph/conn/gpio"
-	"periph.io/x/periph/conn/gpio/gpioreg"
 	"periph.io/x/periph/conn/physic"
 	"periph.io/x/periph/conn/spi"
 	"periph.io/x/periph/conn/spi/spireg"
 	"periph.io/x/periph/host"
 )
-
-func doGpio() {
-	// Make sure periph is initialized.
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Use gpioreg GPIO pin registry to find a GPIO pin by name.
-	p := gpioreg.ByName("GPIO6")
-	if p == nil {
-		log.Fatal("Failed to find GPIO6")
-	}
-
-	// Set it as input, with a pull down (defaults to Low when unconnected) and
-	// enable rising edge triggering.
-	if err := p.In(gpio.PullDown, gpio.RisingEdge); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s is %s\n", p, p.Read())
-
-	// Wait for rising edges (Low -> High) and print when one occur.
-	for p.WaitForEdge(-1) {
-		fmt.Printf("%s went %s\n", p, gpio.High)
-	}
-}
 
 func catchSignals(atexit func()) {
 	sigc := make(chan os.Signal, 1)
@@ -52,41 +24,6 @@ func catchSignals(atexit func()) {
 	if atexit != nil {
 		atexit()
 	}
-}
-
-func doTick() {
-	// tic := time.NewTicker(12675 * time.Nanosecond)
-	tic := time.NewTicker(time.Microsecond)
-	done := make(chan bool, 1)
-	defer tic.Stop()
-	go catchSignals(func() {
-		close(done)
-	})
-	start := time.Now()
-	var cycles uint64
-Loop:
-	for {
-		select {
-		case <-done:
-			// fmt.Println("Done!")
-			break Loop
-		case t := <-tic.C:
-			// fmt.Println("Current time: ", t)
-			_ = t
-		}
-		cycles++
-	}
-
-	d := time.Since(start)
-	// 123 sec 232 cycle
-	// 1   sec   x cycle
-	// -----------------
-	// x = 232 / 123
-	// freq = cycles / elapsed secs
-
-	freq := float64(cycles) / d.Seconds()
-	fmt.Printf("Elapsed %v, done: %d cycles\n", d, cycles)
-	fmt.Printf("Measured toggle freq is %0.3f Hz\n", freq)
 }
 
 func spiConnToReader(conn spi.Conn) (io.Reader, error) {
